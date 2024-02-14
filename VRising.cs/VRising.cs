a672ee247a4d13584a8dbd294ff2c9eeb5275d23
Sarpendon <cernovski@live.de>
2024-02-14 21:24:58 +0100

    Update VRising.cs

--- VRising.cs/VRising.cs
+++ VRising.cs/VRising.cs
@@ -16,68 +16,54 @@ namespace WindowsGSM.Plugins
         // - Plugin Details
         public Plugin Plugin = new Plugin
         {
-            name = "WindowsGSM.VRising", // WindowsGSM.XXXX
+            name = "WindowsGSM.VRising",
             author = "Sarpendon",
             description = "WindowsGSM plugin for supporting VRising Dedicated Server",
             version = "1.1",
-            url = "https://github.com/Sarpendon/WindowsGSM.VRising", // Github repository link (Best practice)
-            color = "#8802db" // Color Hex
+            url = "https://github.com/Sarpendon/WindowsGSM.VRising",
+            color = "#8802db"
         };
 
         // - Settings properties for SteamCMD installer
         public override bool loginAnonymous => true;
-        public override string AppId => "1829350"; // Game server appId
+        public override string AppId => "1829350";
 
         // - Standard Constructor and properties
         public VRising(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
         private readonly ServerConfig _serverData;
         public string Error, Notice;
 
-
         // - Game server Fixed variables
-        public override string StartPath => @"VRisingServer.exe"; // Game server start path
-        public string FullName = "VRising Dedicated Server"; // Game server FullName
-        public bool AllowsEmbedConsole = false;  // Does this server support output redirect?
-        public int PortIncrements = 2; // This tells WindowsGSM how many ports should skip after installation
-        public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
-
+        public override string StartPath => @"VRisingServer.exe";
+        public string FullName = "VRising Dedicated Server";
+        public bool AllowsEmbedConsole = false;
+        public int PortIncrements = 2;
+        public object QueryMethod = new A2S();
 
         // - Game server default values
-        public string Port = "9876"; // UDP port for game traffic, TCP for rcon traffic
-        public string QueryPort = "9877"; // UDP port for Steam server list features
-        public string Defaultmap = "world1"; // Name of save file/directory
-        public string Maxplayers = "10"; // Max number of concurrent players on server
-        public string Additional = "-maxConnectedAdmins 4 -persistentDataPath \".\\save-data\""; // Additional server start parameter
+        public string Port = "9876";
+        public string QueryPort = "9877";
+        public string Defaultmap = "world1";
+        public string Maxplayers = "10";
+        public string Additional = "-maxConnectedAdmins 4 -persistentDataPath \".\\save-data\"";
 
-
-        // - Create a default cfg for the game server after installation
-        public async void CreateServerCFG()
-        {
-             
-        }
-
-        // - Start server function, return its Process to WindowsGSM
         public async Task<Process> Start()
         {
-
-
             string shipExePath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath);
 
             // Prepare start parameter
-			string param = $""; // Set basic parameters
-			param += string.IsNullOrWhiteSpace(_serverData.ServerMap) ? string.Empty : $"-saveName \"{_serverData.ServerMap}\"";
-			param += string.IsNullOrWhiteSpace(_serverData.ServerIP) ? string.Empty : $" -address \"{_serverData.ServerIP}\"";
-			param += string.IsNullOrWhiteSpace(_serverData.ServerName) ? string.Empty : $" -serverName \"{_serverData.ServerName}\"";
-			param += string.IsNullOrWhiteSpace(_serverData.ServerGSLT) ? string.Empty : $" -PrivateServerPassword={_serverData.ServerGSLT}";
-			param += string.IsNullOrWhiteSpace(_serverData.ServerMaxPlayer) ? string.Empty : $" -maxConnectedUsers {_serverData.ServerMaxPlayer}";
-			param += string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $" -gamePort {_serverData.ServerPort}"; 
-			param += string.IsNullOrWhiteSpace(_serverData.ServerQueryPort) ? string.Empty : $" -queryPort {_serverData.ServerQueryPort}";
-			param += string.IsNullOrWhiteSpace(_serverData.ServerParam) ? string.Empty : $" {_serverData.ServerParam}";
-
-
+            string param = $""; 
+            param += string.IsNullOrWhiteSpace(_serverData.ServerMap) ? string.Empty : $"-saveName \"{_serverData.ServerMap}\"";
+            param += string.IsNullOrWhiteSpace(_serverData.ServerIP) ? string.Empty : $" -address \"{_serverData.ServerIP}\"";
+            param += string.IsNullOrWhiteSpace(_serverData.ServerName) ? string.Empty : $" -serverName \"{_serverData.ServerName}\"";
+            param += string.IsNullOrWhiteSpace(_serverData.ServerGSLT) ? string.Empty : $" -PrivateServerPassword={_serverData.ServerGSLT}";
+            param += string.IsNullOrWhiteSpace(_serverData.ServerMaxPlayer) ? string.Empty : $" -maxConnectedUsers {_serverData.ServerMaxPlayer}";
+            param += string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $" -gamePort {_serverData.ServerPort}"; 
+            param += string.IsNullOrWhiteSpace(_serverData.ServerQueryPort) ? string.Empty : $" -queryPort {_serverData.ServerQueryPort}";
+            param += string.IsNullOrWhiteSpace(_serverData.ServerParam) ? string.Empty : $" {_serverData.ServerParam}";
 
             // Prepare Process
-            var p = new Process
+            var gameServerProcess = new Process
             {
                 StartInfo =
                 {
@@ -93,56 +79,73 @@ namespace WindowsGSM.Plugins
             // Set up Redirect Input and Output to WindowsGSM Console if EmbedConsole is on
             if (AllowsEmbedConsole)
             {
-                p.StartInfo.CreateNoWindow = true;
-                p.StartInfo.RedirectStandardInput = true;
-                p.StartInfo.RedirectStandardOutput = true;
-                p.StartInfo.RedirectStandardError = true;
+                gameServerProcess.StartInfo.CreateNoWindow = true;
+                gameServerProcess.StartInfo.RedirectStandardInput = true;
+                gameServerProcess.StartInfo.RedirectStandardOutput = true;
+                gameServerProcess.StartInfo.RedirectStandardError = true;
                 var serverConsole = new ServerConsole(_serverData.ServerID);
-                p.OutputDataReceived += serverConsole.AddOutput;
-                p.ErrorDataReceived += serverConsole.AddOutput;
+                gameServerProcess.OutputDataReceived += serverConsole.AddOutput;
+                gameServerProcess.ErrorDataReceived += serverConsole.AddOutput;
 
                 // Start Process
                 try
                 {
-                    p.Start();
+                    gameServerProcess.Start();
+                }
+                catch (FileNotFoundException e)
+                {
+                    Error = $"File not found: {e.Message}";
+                    return null;
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    Error = $"Access denied: {e.Message}";
+                    return null;
                 }
                 catch (Exception e)
                 {
                     Error = e.Message;
-                    return null; // return null if fail to start
+                    return null;
                 }
 
-                p.BeginOutputReadLine();
-                p.BeginErrorReadLine();
-                return p;
+                gameServerProcess.BeginOutputReadLine();
+                gameServerProcess.BeginErrorReadLine();
+                return gameServerProcess;
             }
 
             // Start Process
             try
             {
-                p.Start();
-                return p;
+                gameServerProcess.Start();
+                return gameServerProcess;
+            }
+            catch (FileNotFoundException e)
+            {
+                Error = $"File not found: {e.Message}";
+                return null;
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Error = $"Access denied: {e.Message}";
+                return null;
             }
             catch (Exception e)
             {
                 Error = e.Message;
-                return null; // return null if fail to start
+                return null;
             }
         }
 
-		// - Stop server function
-     public async Task Stop(Process p)
-		{
-			await Task.Run(() =>
-			{
-				Functions.ServerConsole.SetMainWindow(p.MainWindowHandle);
-				Functions.ServerConsole.SendWaitToMainWindow("^c"); // Send Ctrl+C command
-				p.WaitForExit(5000);
-			});
-		}
+        public async Task Stop(Process gameServerProcess)
+        {
+            await Task.Run(() =>
+            {
+                Functions.ServerConsole.SetMainWindow(gameServerProcess.MainWindowHandle);
+                Functions.ServerConsole.SendWaitToMainWindow("^c"); // Send Ctrl+C command
+                gameServerProcess.WaitForExit(5000);
+            });
+        }
 
-
-        // - Update server function
         public async Task<Process> Update(bool validate = false, string custom = null)
         {
             var (p, error) = await Installer.SteamCMD.UpdateEx(serverData.ServerID, AppId, validate, custom: custom, loginAnonymous: loginAnonymous);
